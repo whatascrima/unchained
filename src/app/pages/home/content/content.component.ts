1ab26e088808cef82be27cc841b7b5646cbf2967
Doplersweb <doplersweb@gmail.com>
2024-06-22 15:50:48 +0100

    up

--- src/app/pages/home/content/content.component.ts
+++ src/app/pages/home/content/content.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -8,4 +8,24 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
 })
-export class ContentComponent {}
+export class ContentComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {
+    document.addEventListener('DOMContentLoaded', function () {
+      const video = document.querySelector(
+        '.background-video'
+      ) as HTMLVideoElement | null;
+      if (video) {
+        video.addEventListener('contextmenu', function (event: MouseEvent) {
+          event.preventDefault();
+        });
+
+        video.play().catch((error) => {
+          // If autoplay fails, try to play the video again with muted attribute.
+          video.muted = true;
+          video.play();
+        });
+      }
+    });
+  }
+}
