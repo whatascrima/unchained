15e215418680bad3735f93629a6e55ac51dc86c9
Doplersweb <doplersweb@gmail.com>
2024-06-23 10:04:36 +0100

    update

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const videos = document.querySelectorAll(
+      '.background-video'
+    ) as NodeListOf<HTMLVideoElement>;
+    videos.forEach((video) => {
+      video.addEventListener('contextmenu', (event: MouseEvent) => {
+        event.preventDefault();
+      });
 
+      video.play().catch((error) => {
+        // If autoplay fails, try to play the video again with muted attribute.
+        video.muted = true;
+        video.play();
+      });
+    });
+    this.mobileVideo();
+  }
+  mobileVideo() {
+    const videos = document.querySelectorAll(
+      '.background-vid'
+    ) as NodeListOf<HTMLVideoElement>;
+    videos.forEach((video) => {
+      video.addEventListener('contextmenu', (event: MouseEvent) => {
+        event.preventDefault();
+      });
+
+      video.play().catch((error) => {
+        // If autoplay fails, try to play the video again with muted attribute.
+        video.muted = true;
+        video.play();
+      });
+    });
+  }
 }
