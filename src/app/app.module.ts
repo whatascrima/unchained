fffc0453932fff7d9971ae165ff121e0adc1b244
Doplersweb <doplersweb@gmail.com>
2024-06-23 11:18:32 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeModule } from './pages/home/home.module';
 import { CollectionsModule } from './pages/collections/collections.module';
 import { PreloaderComponent } from './_components/preloader/preloader.component';
+import { LoginModule } from './pages/login/login.module';
+import { RegisterModule } from './pages/register/register.module';
 
 @NgModule({
   declarations: [AppComponent, PreloaderComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule, CollectionsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    CollectionsModule,
+    LoginModule,
+    RegisterModule,
+  ],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent],
